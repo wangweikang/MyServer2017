6feec24dc9200f44098f42de00d50500172c8d87
Will <kangmang92@foxmail.com>
2017-05-13 17:48:12 +0800

    lol

--- wsgi.py
+++ wsgi.py
@@ -8,23 +8,3 @@ import app
 
 sys.path.insert(0, abspath(dirname(__file__)))
 application = app.app
-
-"""
-建立一个软连接
-ln -s /var/www/bbs/MyServer/bbs.conf /etc/supervisor/conf.d/bbs.conf
-ln -s /var/www/bbs/MyServer/bbs.nginx /etc/nginx/sites-enabled/bbs
-
-
-➜  ~ cat /etc/supervisor/conf.d/bbs.conf
-
-[program:bbs]
-command=/usr/local/bin/gunicorn wsgi -c gunicorn.config.py
-directory=/var/www/MyServer
-autostart=true
-autorestart=true
-
-
-/usr/local/bin/gunicorn wsgi
---bind 0.0.0.0:2001
---pid /tmp/bbs.pid
-"""
