433d222a379bd67da67ef0b3be3760c104574731
Will <kangmang92@foxmail.com>
2017-05-16 16:59:02 +0800

    reply

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,6 +4,9 @@ from models.user import User
 
 
 def current_user():
-    uid = session.get('user_id', '')
-    u = User.find_by(id=int(uid))
+    # 从 session 中找到 user_id 字段, 找不到就 -1
+    # 然后 User.find_by 来用 id 找用户
+    # 找不到就返回 None
+    uid = session.get('user_id', -1)
+    u = User.find_by(id=uid)
     return u
--- routes/reply.py
+++ routes/reply.py
@@ -17,7 +17,8 @@ main = Blueprint('reply', __name__)
 def add():
     form = request.form
     u = current_user()
-    # log('DEBUG', form)
-    m = Reply.new(form)
-    m.set_user_id(u.id)
-    return redirect(url_for('topic.detail', id=m.topic_id))
+    if u is not None:
+        # log('DEBUG', form)
+        m = Reply.new(form)
+        m.set_user_id(u.id)
+        return redirect(url_for('topic.detail', id=m.topic_id))
