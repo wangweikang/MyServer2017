92f910318801bcb0100d22b88c1f43d006db06e1
Will <kangmang92@foxmail.com>
2017-08-23 16:40:05 +0800

    rubik

--- static/js/index.js
+++ static/js/index.js
@@ -23,13 +23,14 @@ Wrap.onmousedown = function (ev) {
 
 Wrap.ontouchstart = function (ev) {
     var oEvent = ev || event;
-    var disX = oEvent.clientX - y;
-    var disY = oEvent.clientY - x;
-
+    var disX = oEvent.changedTouches[0].clientX - y;
+    var disY = oEvent.changedTouches[0].clientY - x;
+    console.log(oEvent.changedTouches[0]);
     document.ontouchmove = function (ev) {
         var oEvent = ev || event;
-        x = oEvent.clientY - disY;
-        y = oEvent.clientX - disX;
+        // console.log(oEvent);
+        x = oEvent.changedTouches[0].clientY - disY;
+        y = oEvent.changedTouches[0].clientX - disX;
 
         Wrap.style.transform = 'perspective(800px) rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
     };
