5c0a75c3e15f555e0a08272563b6b84aa43eb187
Will <kangmang92@foxmail.com>
2017-08-23 16:00:59 +0800

    rubik

--- static/js/index.js
+++ static/js/index.js
@@ -1,24 +1,41 @@
+var Wrap = document.querySelector('#wrap');
+var y = -60;
+var x = 45;
 
-var Wrap=document.querySelector('#wrap');
-var y=-60;
-var x=45;
+Wrap.onmousedown = function (ev) {
+    var oEvent = ev || event;
+    var disX = oEvent.clientX - y;
+    var disY = oEvent.clientY - x;
 
-Wrap.onmousedown=function(ev)
-{
-    var oEvent=ev||event;
-    var disX=oEvent.clientX-y;
-    var disY=oEvent.clientY-x;
+    document.onmousemove = function (ev) {
+        var oEvent = ev || event;
+        x = oEvent.clientY - disY;
+        y = oEvent.clientX - disX;
 
-    document.onmousemove=function(ev){
-        var oEvent=ev||event;
-        x=oEvent.clientY-disY;
-        y=oEvent.clientX-disX;
-
-        Wrap.style.transform='perspective(800px) rotateX('+x+'deg) rotateY('+y+'deg)';
+        Wrap.style.transform = 'perspective(800px) rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
     };
-    document.onmouseup=function(){
-        document.onmousemove=null;
-        document.onmouseup=null;
+    document.onmouseup = function () {
+        document.onmousemove = null;
+        document.onmouseup = null;
     };
-    return false; 
+    return false;
+};
+
+Wrap.ontouchstart = function (ev) {
+    var oEvent = ev || event;
+    var disX = oEvent.clientX - y;
+    var disY = oEvent.clientY - x;
+
+    document.ontouchmove = function (ev) {
+        var oEvent = ev || event;
+        x = oEvent.clientY - disY;
+        y = oEvent.clientX - disX;
+
+        Wrap.style.transform = 'perspective(800px) rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
+    };
+    document.ontouchend = function () {
+        document.ontouchmove = null;
+        document.ontouchend = null;
+    };
+    return false;
 };
